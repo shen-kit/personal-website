9856f506a205dd234ddb8683728fe4d2f5fd73bb
Shen-Kit <shenkit108+1@gmail.com>
2022-12-20 10:25:08 +0800

    changed homepage route path to /personal-website from /

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route path='/' element={ <Home /> } />
+        <Route path='/personal-website' element={ <Home /> } />
         <Route path='/about' element={ <About /> } />
         <Route path='/achievements' element={ <Achievements /> } />
         <Route path='/projects' element={ <Projects /> } />
