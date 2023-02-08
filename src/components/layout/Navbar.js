220df8fd2394b015b674bd1e4b9deebf263cacf1
Shen-Kit <shenkit108+1@gmail.com>
2023-02-08 13:07:11 +0800

    created projects page (bujo, life is short, and password manager). Also increased paragraph line spacing.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
         <li><Link to='/about'>About Me</Link></li>
         <li><Link to='/achievements'>Achievements</Link></li>
         <li><Link to='/projects'>Projects</Link></li>
-        <li><Link to='/blog'>Blog</Link></li>
+        <li><Link to='/services'>Services</Link></li>
         <li className={classes.resume}>Resume</li>
       </ul>
     </header>
--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -5,12 +5,8 @@ function Achievements() {
   return (
     <div className={classes.center}>
       <Heading number='1' text='Achievements and Awards' />
+      <h2>WACE/ATAR</h2>
       <ul className={classes.mainList}>
-        <li><strong>General Exhibition</strong> (2022)</li>
-        <li>Engineer's Award WA (2022)</li>
-        <li><strong>Chemistry Subject Exhibition</strong> (2021)</li>
-        <li>2 ICAS Science medals (2019 & 2020)</li>
-        <li>Accepted to the Australian Chemistry Olympiad Summer School (29 out of over 4000)</li>
         <li>Graduated with a <strong>99.95 ATAR</strong> and final grades of:
           <ul>
             <li>100.0% for Mathematics Specialist</li>
@@ -21,7 +17,32 @@ function Achievements() {
             <li>84.3% for Literature</li>
           </ul>
         </li>
+        <li><strong>Chemistry Subject Exhibition</strong> (top student in the state)</li>
+        <li><strong>General Exhibition</strong> (14th in the state)</li>
       </ul>
+
+      <h2>School Awards</h2>
+      <ul className={classes.mainList}>
+        <li>Academic Excellence Award</li>
+        <li>Top 3 in Chemistry, Mathematics Methods, Mathematics Specialist, and Physics</li>
+        <li>Platinum House Award (achieved in Year 10)</li>
+      </ul>
+
+      <h2>Co-Curricular Activities</h2>
+      <ul>
+        <li>Played in the Honours Division for interschool volleyball (2022)</li>
+        <li>Won 2 ICAS Science medals in 2019 & 2021 (top in the state)</li>
+        <li>State finalist in the Microsoft AI For Good Challenge (2020)</li>
+      </ul>
+
+      <h2>University Scholarship Offers</h2>
+      <ul className={classes.mainList, classes.lastList}>
+        <li><strong>UNSW</strong>: Scientia Scholarship</li>
+        <li><strong>University of Melbourne</strong>: Chancellor's Scholarship</li>
+        <li><strong>Monash</strong>: Sir John Monash Scholarship for Distinction</li>
+        <li><strong>UWA</strong>: Lawrence Scholarship</li>
+        <li><strong>Curtin</strong>: Top Excellence Scholarship</li>
+      </ul> 
     </div>
   )
 }
