774aa11ece32256872aef1636815971956ed3318
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-04-11 11:56:11 +1000

    updated Achievements page

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -32,25 +32,27 @@ function Achievements() {
       <ul className={classes.mainList}>
         <li>Academic Excellence Award</li>
         <li>Top 3 in Chemistry, Mathematics Methods, Mathematics Specialist, and Physics</li>
-        <li>Platinum House Award (achieved in Year 10)</li>
+        <li>Platinum House Award (achieved in 2020)</li>
       </ul>
 
-      <h2>Co-Curricular Activities</h2>
+      <h2>Co-Curricular</h2>
       <ul>
-        <li>Played in the Honours Division for interschool volleyball (2022)</li>
-        <li>Formed a barbershop quartet with 3 friends, we performed at primary schools and eventually at our own graduation ceremony</li>
-        <li>Won 2 ICAS Science medals in 2019 & 2021 (top in the state)</li>
+        <li>Played in the Honours division for interschool volleyball (2022)</li>
+        <li><strong>Started a barbershop quartet</strong> with 3 friends, we performed at primary schools and eventually at our own graduation ceremony (2022)</li>
+        <li>Won <strong>2 ICAS Science medals</strong> in 2019 & 2021 (top in the state)</li>
         <li>State finalist in the Microsoft AI For Good Challenge (2020)</li>
-        <li>Completed over <strong>110 hours of community service</strong> during senior highschool (Years 10-12)</li>
+        <li>Completed over <strong>110 hours of community service</strong> during senior highschool (2020-2022)</li>
+        <li>Section leader in the Symphony Orchestra and Senior Wind Orchestra (2022)</li>
+        <li>Member of the Symphony Orchestra, Senior Wind Orchestra, Boys Chamber Choir, Mixed Chamber Choir, and Chorale (varied from 2019-2022)</li>
       </ul>
 
       <h2>University Scholarship Offers</h2>
       <ul className={classes.mainList + " " + classes.lastList}>
         <li><strong>UNSW</strong>: Scientia Scholarship</li>
         <li><strong>University of Melbourne</strong>: Chancellor's Scholarship</li>
-        <li><strong>Monash</strong>: Sir John Monash Scholarship for Distinction</li>
+        <li><strong>Monash University</strong>: Sir John Monash Scholarship for Distinction</li>
         <li><strong>UWA</strong>: Lawrence Scholarship</li>
-        <li><strong>Curtin</strong>: Top Excellence Scholarship</li>
+        <li><strong>Curtin University</strong>: Top Excellence Scholarship</li>
       </ul> 
 
     </div>
