2650c37810e2ae98a451928614e46e3be818d57a
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-04-11 12:27:52 +1000

    updated About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,8 +14,8 @@ function About() {
              service soup kitchens, and academic competitions. He loves researching and experimenting with
              productivity in the hopes of achieving as much as he possibly can. This has allowed him to excel
              academically while simultaneously spending 10 hours each week at music rehearsals, teaching himself
-             how to code, starting a YouTube channel, regularly making time for social activities, and working
-             out to improve his fitness and health.
+             how to code, starting a YouTube channel (with over 400K views), regularly making time for social
+             activities, and working out to improve his fitness and health.
         </p>
       </section>
       <div>
