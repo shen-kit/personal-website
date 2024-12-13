a0d7e163b4eae1fd0729ceead7ce128b0919bc3e
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-12-13 12:22:10 +0800

    adds achievements and kitkat language to projects

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -1,20 +1,28 @@
-import Heading from '../components/Heading.js';
-import classes from './Achievements.module.css';
+import Heading from "../components/Heading.js";
+import classes from "./Achievements.module.css";
 
 function Achievements() {
   return (
     <div className={classes.center}>
-      <Heading number='1' text='Achievements and Awards' />
+      <Heading number="1" text="Achievements and Awards" />
 
       <h2>University</h2>
       <ul className={classes.mainList}>
-        <li><strong>1st place</strong> in the Monash Association of Coding UI/UX Design Hackathon (2024)</li>
-        <li>Currently maintaining a perfect GPA and 93 WAM</li>
+        <li>
+          <strong>1st place</strong> in the Monash Beginner Competitive
+          Programming Competition (2024)
+        </li>
+        <li>
+          <strong>1st place</strong> in the Monash Association of Coding UI/UX
+          Design Hackathon (2024)
+        </li>
+        <li>Currently maintaining a 4.0 GPA and 94 WAM</li>
       </ul>
 
       <h2>WACE / ATAR</h2>
       <ul className={classes.mainList}>
-        <li>Graduated with a <strong>99.95 ATAR</strong> and final grades of:
+        <li>
+          Graduated with a <strong>99.95 ATAR</strong> and final grades of:
           <ul>
             <li>100.0% for Mathematics Specialist</li>
             <li>99.3% for Chemistry</li>
@@ -24,40 +32,76 @@ function Achievements() {
             <li>84.3% for Literature</li>
           </ul>
         </li>
-        <li><strong>Chemistry Subject Exhibition</strong> (top student in the state)</li>
-        <li><strong>General Exhibition</strong> (14th in the state)</li>
+        <li>
+          <strong>Chemistry Subject Exhibition</strong> (top student in the
+          state)
+        </li>
+        <li>
+          <strong>General Exhibition</strong> (14th in the state)
+        </li>
       </ul>
 
       <h2>School Awards</h2>
       <ul className={classes.mainList}>
         <li>Academic Excellence Award</li>
-        <li>Top 3 in Chemistry, Mathematics Methods, Mathematics Specialist, and Physics</li>
+        <li>
+          Top 3 in Chemistry, Mathematics Methods, Mathematics Specialist, and
+          Physics
+        </li>
         <li>Platinum House Award (achieved in 2020)</li>
       </ul>
 
       <h2>Co-Curricular</h2>
-      <ul>
-        <li>Played in the Honours division for interschool volleyball (2022)</li>
-        <li><strong>Started a barbershop quartet</strong> with 3 friends, we performed at primary schools and eventually at our own graduation ceremony (2022)</li>
-        <li>Won <strong>2 ICAS Science medals</strong> in 2019 & 2021 (top in the state)</li>
-        <li>State finalist in the Microsoft AI For Good Challenge (2020)</li>
-        <li>Completed over <strong>110 hours of community service</strong> during senior highschool (2020-2022)</li>
-        <li>Section leader in the Symphony Orchestra and Senior Wind Orchestra (2022)</li>
-        <li>Member of the Symphony Orchestra, Senior Wind Orchestra, Boys Chamber Choir, Mixed Chamber Choir, and Chorale (varied from 2019-2022)</li>
+      <ul className={classes.mainList}>
+        <li>
+          Played in the Honours division for interschool volleyball (2022)
+        </li>
+        <li>
+          <strong>Started a barbershop quartet</strong>, performed at primary
+          schools and our own graduation ceremony (2022)
+        </li>
+        <li>
+          Completed over <strong>110 hours of community service</strong> during
+          senior highschool (2020-2022)
+        </li>
+        <li>
+          Section leader in the Symphony Orchestra and Senior Wind Orchestra
+          (2022)
+        </li>
+        <li>
+          Member of the Symphony Orchestra, Senior Wind Orchestra, Boys Chamber
+          Choir, Mixed Chamber Choir, and Chorale (varied from 2019-2022)
+        </li>
+        <li>
+          Won <strong>2 ICAS Science medals</strong> (top in the state) (2019,
+          2021)
+        </li>
+        <li>
+          Came second in the state in the Microsoft AI For Good Challenge (2020)
+        </li>
       </ul>
 
       <h2>University Scholarship Offers</h2>
       <ul className={classes.mainList + " " + classes.lastList}>
-        <li><strong>UNSW</strong>: Scientia Scholarship</li>
-        <li><strong>University of Melbourne</strong>: Chancellor's Scholarship</li>
-        <li><strong>Monash University</strong>: Sir John Monash Scholarship for Distinction</li>
-        <li><strong>UWA</strong>: Lawrence Scholarship</li>
-        <li><strong>Curtin University</strong>: Top Excellence Scholarship</li>
-      </ul> 
-
+        <li>
+          <strong>UNSW</strong>: Scientia Scholarship
+        </li>
+        <li>
+          <strong>University of Melbourne</strong>: Chancellor's Scholarship
+        </li>
+        <li>
+          <strong>Monash University</strong>: Sir John Monash Scholarship for
+          Distinction
+        </li>
+        <li>
+          <strong>UWA</strong>: Lawrence Scholarship
+        </li>
+        <li>
+          <strong>Curtin University</strong>: Top Excellence Scholarship
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Achievements;
-
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,33 +1,75 @@
-import classes from './Projects.module.css';
-import Heading from '../components/Heading.js';
+import classes from "./Projects.module.css";
+import Heading from "../components/Heading.js";
 
 function Projects() {
   return (
     <div>
+      <Heading number="2" text="Projects" />
 
-      <Heading number='2' text="Projects" />
-
+      <section className={classes.section}>
+        <iframe
+          src="https://www.youtube.com/embed/Wib5VxiJf9Y?si=bPnEf_tJ4oiLJxiQ"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+        <div>
+          <h2 className={classes.h2}>KitKat Language | 2024</h2>
+          <p>
+            I created my own high-level, statically and strongly typed, compiled
+            programming language using Haskell. It compiles to LLVM IR (a
+            processor-independent intermediate representation), then uses{" "}
+            <tt>llc</tt> and <tt>gcc</tt> to link to the standard C libraries
+            and generate machine code. Functionality includes
+            integers/strings/booleans, conditionals (if/else if/else, while
+            loops), boolean logic and integer arithmetic, print statements, and
+            variables for all data types.
+          </p>
+        </div>
+      </section>
       <section className={classes.section}>
         <div>
           <h2 className={classes.h2}>Hia's Up | 2023</h2>
           <p>
-            Inspired by the game <a href='https://play.google.com/store/apps/details?id=com.wb.headsup&hl=en_AU&gl=US'>Head's Up!</a>,
-            I created <i>Hia's Up!</i> as a game that we could play at family gatherings. The game is like celebrity heads, where the
-            player has to guess what is on the screen by asking everyone else questions. If they guess it right, they tilt the phone up,
-            and to skip, they tilt the phone down. It was made using Flutter and Dart (source code available on <a href='https://github.com/shen-kit/heads-up-game'>github</a>).
+            Inspired by the game{" "}
+            <a href="https://play.google.com/store/apps/details?id=com.wb.headsup&hl=en_AU&gl=US">
+              Head's Up!
+            </a>
+            , I created <i>Hia's Up!</i> as a game that we could play at family
+            gatherings. The game is like celebrity heads, where the player has
+            to guess what is on the screen by asking everyone else questions. If
+            they guess it right, they tilt the phone up, and to skip, they tilt
+            the phone down. It was made using Flutter and Dart (source code
+            available on{" "}
+            <a href="https://github.com/shen-kit/heads-up-game">github</a>).
           </p>
         </div>
-        <iframe src="https://youtube.com/embed/hdXAF2hohBY" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe
+          src="https://youtube.com/embed/hdXAF2hohBY"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
       </section>
       <section className={classes.section}>
-        <iframe src="https://www.youtube.com/embed/vpSjNAjYRa4?controls=1&amp;start=39" title="Bujo App Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe
+          src="https://www.youtube.com/embed/vpSjNAjYRa4?controls=1&amp;start=39"
+          title="Bujo App Video"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
         <div>
           <h2 className={classes.h2}>Bujo App | 2021</h2>
           <p>
-            This was my final project for the CS50 Introduction to Computer Science course, written in HTML, CSS and Javascript, and using
-            the Electron framework. Features include a daily planner with tasks and events, a monthly calendar, and a habit tracker which
-            integrates with the calendar. This project taught me web-development skills, how to use npm, how to write responsive apps, and
-            the importance of creating a thorough plan before starting to code. The source code is available on <a href='https://github.com/shen-kit/bujoApp'>github</a>.
+            This was my final project for the CS50 Introduction to Computer
+            Science course, written in HTML, CSS and Javascript, and using the
+            Electron framework. Features include a daily planner with tasks and
+            events, a monthly calendar, and a habit tracker which integrates
+            with the calendar. This project taught me web-development skills,
+            how to use npm, how to write responsive apps, and the importance of
+            creating a thorough plan before starting to code. The source code is
+            available on{" "}
+            <a href="https://github.com/shen-kit/bujoApp">github</a>.
           </p>
         </div>
       </section>
@@ -36,21 +78,39 @@ function Projects() {
         <div>
           <h2 className={classes.h2}>Life is Short | 2020</h2>
           <p>
-            In 2020 I challenged myself to make and release a game to the public in only 4 weeks. This survival game was the result -  it was made
-            in Unity and features a flexible weapon system and various enemies, as well as some unique mechanics. While it is far from polished, I
-            was extremely happy what I accomplished in 4 weeks, and this video documents the process of making the game. It is free to download <a href='https://sk-studios.itch.io/life-is-short'>here</a>.
+            In 2020 I challenged myself to make and release a game to the public
+            in only 4 weeks. This survival game was the result - it was made in
+            Unity and features a flexible weapon system and various enemies, as
+            well as some unique mechanics. While it is far from polished, I was
+            extremely happy what I accomplished in 4 weeks, and this video
+            documents the process of making the game. It is free to download{" "}
+            <a href="https://sk-studios.itch.io/life-is-short">here</a>.
           </p>
         </div>
-        <iframe src="https://www.youtube.com/embed/k9yz5tZ35-k" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe
+          src="https://www.youtube.com/embed/k9yz5tZ35-k"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
       </section>
 
       <section className={classes.section}>
-        <iframe frameborder="0" src="https://itch.io/embed/600889?bg_color=252525&amp;fg_color=ffffff&amp;border_color=111111" width="552" height="167"><a href="https://sk-studios.itch.io/stealth">Stealth by SK Studios</a></iframe>
+        <iframe
+          frameborder="0"
+          src="https://itch.io/embed/600889?bg_color=252525&amp;fg_color=ffffff&amp;border_color=111111"
+          width="552"
+          height="167"
+        >
+          <a href="https://sk-studios.itch.io/stealth">Stealth by SK Studios</a>
+        </iframe>
         <div>
           <h2 className={classes.h2}>Stealth | 2020</h2>
           <p>
-            From 2017-2020, I made many games. This game is relatively simple compared to Life is Short (above), but is my most popular one, with over 3000 plays online (across multiple websites)
-            and 100 downloads. It was made in Unity3D using C#.
+            From 2017-2020, I made many games. This game is relatively simple
+            compared to Life is Short (above), but is my most popular one, with
+            over 3000 plays online (across multiple websites) and 100 downloads.
+            It was made in Unity3D using C#.
           </p>
         </div>
       </section>
@@ -58,23 +118,29 @@ function Projects() {
       <section>
         <h2 className={classes.h2}>CLI Password Manager | 2023</h2>
         <p className={classes.lastParagraph}>
-          I've made several password managers over the years, the most recent one in 2023 in Python using
-          the Cryptography library for security. I decided to make it to learn the basics of cryptography and to refresh my knowledge of databases
-          (I used sqlite). Additionally, I enjoy working in the terminal so didn't want a GUI password manager. Functionality includes setting a
-          master password which is then encrypted, adding/editing/deleting new services, listing all stored services, and resetting the database.
-          While this wasn't very challenging to build, it is the project which I use the most in my daily life.
+          I've made several password managers over the years, the most recent
+          one in 2023 in Python using the Cryptography library for security. I
+          decided to make it to learn the basics of cryptography and to refresh
+          my knowledge of databases (I used sqlite). Additionally, I enjoy
+          working in the terminal so didn't want a GUI password manager.
+          Functionality includes setting a master password which is then
+          encrypted, adding/editing/deleting new services, listing all stored
+          services, and resetting the database. While this wasn't very
+          challenging to build, it is the project which I use the most in my
+          daily life.
         </p>
       </section>
 
       <section>
         <h2 className={classes.h2}>Personal Website | 2023</h2>
         <p className={classes.lastParagraph}>
-          Having never created a website before, I decided to make this one as a way to teach myself the basics of ReactJS and how to
-          deploy a website online. No templates were used, and all of the code for this website was written myself. The source code
-          can be found on <a href='https://github.com/shen-kit/personal-website'>github</a>.
+          Having never created a website before, I decided to make this one as a
+          way to teach myself the basics of ReactJS and how to deploy a website
+          online. No templates were used, and all of the code for this website
+          was written myself. The source code can be found on{" "}
+          <a href="https://github.com/shen-kit/personal-website">github</a>.
         </p>
       </section>
-
     </div>
   );
 }
