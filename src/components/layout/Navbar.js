744de5938a25d358bf4e733b9dbe7c50e6679e87
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-12-13 17:49:23 +0800

    adds resume download link to navbar and renames resume file

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import classes from './Navbar.module.css';
+import classes from "./Navbar.module.css";
 
 function Navbar() {
   return (
     <header>
-      <Link className={classes.logo} to='/personal-website'>SK</Link>
+      <Link className={classes.logo} to="/personal-website">
+        SK
+      </Link>
       <ul>
-        <li><Link to='/about'>About Me</Link></li>
-        <li><Link to='/achievements'>Achievements</Link></li>
-        <li><Link to='/projects'>Projects</Link></li>
+        <li>
+          <Link to="/about">About Me</Link>
+        </li>
+        <li>
+          <Link to="/achievements">Achievements</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <a href="https://downgit.github.io/#/home?url=https://github.com/shen-kit/personal-website/blob/main/shenkit_resume_2024.pdf">
+            Resume
+          </a>
+        </li>
       </ul>
     </header>
   );
