e1183f04174215877388289a2f716895f452c336
Shen-Kit Hia <shenkit108@gmail.com>
2025-04-14 15:58:52 +1000

    updates about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,25 +1,29 @@
-import classes from './About.module.css';
-import Heading from '../components/Heading.js';
+import classes from "./About.module.css";
+import Heading from "../components/Heading.js";
 
-import image from '../assets/oahs.jpg';
+import image from "../assets/oahs.jpg";
 
 function About() {
   return (
     <div className={classes.center}>
       <section>
-        <Heading number='0' text='About Me' />
+        <Heading number="0" text="About Me" />
         <p className={classes.p}>
-             Shen-Kit graduated from Perth Modern School in 2022 with an ATAR of 99.95. Throughout highschool
-             he took part in many extracurricular activities, including music, basketball, volleyball, community
-             service soup kitchens, and academic competitions. He loves researching and experimenting with
-             productivity in the hopes of achieving as much as he possibly can. This has allowed him to excel
-             academically while simultaneously spending 10 hours each week at music rehearsals, teaching himself
-             how to code, starting a YouTube channel (with over 400K views), regularly making time for social
-             activities, and working out to improve his fitness and health.
+          Shen-Kit graduated from Perth Modern School in 2022 with an ATAR of
+          99.95, and is currently studying Software Engineering at Monash
+          University. Throughout highschool he took part in many extracurricular
+          activities including music, basketball, volleyball, community service
+          soup kitchens, and academic competitions. He loves researching and
+          experimenting with productivity in the hopes of achieving as much as
+          he possibly can. This has allowed him to excel academically while
+          simultaneously playing in 6 ensembles, teaching himself to code,
+          starting a YouTube channel (with 500K views), regularly making time
+          for social activities, and working out to improve his fitness and
+          health.
         </p>
       </section>
       <div>
-        <img className={classes.myImg} src={image} alt='me doing a handstand' />
+        <img className={classes.myImg} src={image} alt="me doing a handstand" />
       </div>
     </div>
   );
